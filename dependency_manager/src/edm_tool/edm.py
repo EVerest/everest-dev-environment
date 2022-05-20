2c36419361d0cb71ab8dd344835733d24878c6d6
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2022-05-20 13:25:39 +0200

    Removed superfluous check for __main__
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- dependency_manager/src/edm_tool/edm.py
+++ dependency_manager/src/edm_tool/edm.py
@@ -975,7 +975,3 @@ def main(parser: argparse.ArgumentParser):
     checkout = EDM.checkout_local_dependencies(workspace, args.workspace, dependencies)
     EDM.write_cmake(workspace, checkout, dependencies, out_file)
 
-
-if __name__ == "__main__":
-    parser = get_parser()
-    main(parser)
